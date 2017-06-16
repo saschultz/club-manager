aa11485bf7843d3e910dd33221351c6ae910e818
Sara Schultz <saschultz95@gmail.com>
2017-06-16 11:48:02 -0700

    create member.service and import member model and firebase data

---
+++ src/app/member.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Member } from './member.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+
+@Injectable()
+export class MemberService {
+
+  constructor() { }
+
+}

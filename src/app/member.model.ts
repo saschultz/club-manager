4605569de2a9db43b20740149737d834ed2c877d
Sara Schultz <saschultz95@gmail.com>
2017-06-16 11:03:28 -0700

    add member.model.ts with Member constructor

---
+++ src/app/member.model.ts
@@ -0,0 +1,9 @@
+export class Member {
+  constructor (
+    public name: string,
+    public bio: string,
+    public medium: string,
+    public image: string,
+    public email: string
+  ) { }
+}

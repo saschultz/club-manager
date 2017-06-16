2b57f99f1a0acaca85ecec20b56a4dc32376daf1
Sara Schultz <saschultz95@gmail.com>
2017-06-16 11:55:38 -0700

    import FirebaseListObservable in member component

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Member } from '../member.model';
 import { Router } from '@angular/router';
 import { MemberService } from '../member.service';
-
-
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-members',
@@ -12,6 +11,7 @@ import { MemberService } from '../member.service';
   providers: [MemberService]
 })
 export class MemberComponent implements OnInit {
+  members: FirebaseListObservable<any[]>;
 
   constructor(private router: Router, private memberService: MemberService) { }
 
